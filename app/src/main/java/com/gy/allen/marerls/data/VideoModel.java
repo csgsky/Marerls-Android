c840625072ff4b6be2accba37332db5d36e6f383
csgsky <674143140@qq.com>
2018-02-11 10:54:11 +0800

    remove video

---
+++ app/src/main/java/com/gy/allen/marerls/data/VideoModel.java
@@ -0,0 +1,8 @@
+package com.gy.allen.marerls.data;
+
+/**
+ * Created by allen on 18/1/30.
+ */
+
+public class VideoModel {
+}
--- app/src/main/java/com/gy/allen/marerls/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/gy/allen/marerls/ui/fragment/AboutFragment.java
@@ -6,9 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import com.gy.allen.marerls.MainActivity;
 import com.gy.allen.marerls.R;
 
 /**
@@ -18,23 +16,19 @@ import com.gy.allen.marerls.R;
 public class AboutFragment extends Fragment {
 
     private View mView;
-    private MainActivity activity;
 
-    public static final int RESULT_CODE_CHOOSE = 100;
-    private Button btn_choose_photo;
+    public AboutFragment() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         mView = View.inflate(getActivity(), R.layout.fragment_about, null);
-        activity = (MainActivity) getActivity();
-        initView();
         return mView;
-    }
-
-    private void initView() {
-        btn_choose_photo = mView.findViewById(R.id.btn_choose_photo);
-
     }
 
 }
